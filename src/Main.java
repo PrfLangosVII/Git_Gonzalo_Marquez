4935077ffee883ca93cf6f9469ab0a49a687b27d
PrfLangosVII <96775390+PrfLangosVII@users.noreply.github.com>
2024-11-25 10:24:52 +0100

    "Modificación 5"

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Alumno:_Gonzalo_Márquez");
 		System.out.println("Modificación desde Master - Cambiado");
+		System.out.println("Modificación desde GitHub");
 	}
 }
