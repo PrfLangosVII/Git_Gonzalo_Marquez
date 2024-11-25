8a2818b1afb75e7a7b69fd628ae6c442daa5c527
Gonzalo <gmarquezlaes@gmail.com>
2024-11-25 09:50:46 +0100

    "Modificación 3"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Alumno:_Gonzalo_Márquez");
-		System.out.println("Primera Modificación desde Master");
+		System.out.println("Modificación desde Master - Cambiado");
 	}
 }
